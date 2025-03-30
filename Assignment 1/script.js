3eb53eef0e4dac5c164424ee6d92fc41839f9f71
ropetightightrope <santino.castagna@gmail.com>
2025-03-30 16:09:43 +1100

    folder

---
+++ Assignment 1/script.js
@@ -0,0 +1,9 @@
+const button1 = document.getElementById(button1)
+let audio1 = new Audio ();
+audio1.src = 'Bees.mp3';
+
+button1.addEventListener("click", function(){
+    audio1.play();
+    audio1.addEventListener
+    
+}
